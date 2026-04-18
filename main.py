f645e4c37a5670f809bf8c208b33fef97f706cf9
jennyenamorado436-max <jennyenamorado436@gmail.com>
2026-04-18 16:14:23 -0600

    Corrijo error de despliegue

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hola, chatbot listo!"}
+
 def responder(mensaje):
     if "hola" in mensaje.lower():
         return "¡Hola! ¿Cómo estás?"
